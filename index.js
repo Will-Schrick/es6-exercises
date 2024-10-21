fde3c55fe595cfb7b5a1cdac037ffef8f14d11a4
Will-S <willschrick@gmail.com>
2024-10-21 11:56:37 -0700

    almost done

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
-const rectangleArea = /* Añade tu código aquí */;
+//1
+const rectangleArea = (largo,ancho) =>  largo * ancho; /* Añade tu código aquí */
 
-const greet = (nombre) => /* Añade tu código aquí */;
+ 
+//2
+const greet = (nombre) => "Hola, " + nombre + "!";
 
-const mergeArray = (arr1, arr2) => /* Añade tu código aquí */;
+ 
+//3
+const mergeArray = (arr1, arr2) =>  [...arr1, ...arr2];
 
-const ducplicateAndSum = (a, b, /* Añade tu código aquí */) => {
-  const suma = a + b;
-  return numeros.map((numero) => numero + suma);
+
+//4 
+const ducplicateAndSum  = (a, b, ...numeros) => {
+   const suma = a + b;
+   return numeros.map((numero) => numero + suma);
 };
 
+//5
 const setExercise = () => {
-  /* Añade tu código aquí */
-};
+   //Añade tu código aquí
+   const mySet = new Set([0,0,1,1,2,2,3,4,5]);
+   return mySet.size;
+   };
+   console.log(setExercise());
+   
+   
+   
+ 
 
+
+//6
 const miMapa = new Map();
 miMapa.set("nombre", "Juan");
 miMapa.set("edad", 30);
 miMapa.set("profesion", "Desarrollador");
 
+
+
+
 const mapExercise = () => {
-  /* Añade tu código aquí */
+   //Añade tu código aquí
+   
+   miMapa.set("profesion", "Genius");
+   miMapa.delete("edad");
 };
 
+
+//8
 const countRepeated = (string) => {
-  /* Añade tu código aquí */
+   //Añade tu código aquí 
 };
+
